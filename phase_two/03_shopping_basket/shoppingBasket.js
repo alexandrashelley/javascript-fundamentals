ef8ea2b2b964148087ef2b160f2fc5c998f81da6
alexandrashelley <alexbshelley@gmail.com>
2022-08-25 11:55:37 +0100

    completed shopping basket challenge applyDiscount

--- phase_two/03_shopping_basket/shoppingBasket.js
+++ phase_two/03_shopping_basket/shoppingBasket.js
@@ -1,17 +1,21 @@
 class ShoppingBasket {
   constructor() {
     this.basket = []
+    this.discount = 0;
   }
   getTotalPrice() {
     let totalPrice = 0
     this.basket.forEach((candy) => totalPrice += candy.getPrice());
-    return totalPrice;
-    // return 0;
+    return +(totalPrice - this.discount).toFixed(2);
   };
 
   addItem(candy) {
     this.basket.push(candy);
-  }
+  };
+
+  applyDiscount(discount) {
+    this.discount += discount;
+  };
 };
 
 module.exports = ShoppingBasket;
--- phase_two/03_shopping_basket/shoppingBasket.test.js
+++ phase_two/03_shopping_basket/shoppingBasket.test.js
@@ -1,20 +1,24 @@
-const ShoppingBasket = require('./shoppingBasket')
+const ShoppingBasket = require("./shoppingBasket");
 // const Candy = require('./candy')
 
 describe("the ShoppingBasket object", () => {
-  it('returns 0 for price with an empty basket', () => {
-  const basket = new ShoppingBasket;
+  it("returns 0 for price with an empty basket", () => {
+    const basket = new ShoppingBasket();
     expect(basket.getTotalPrice()).toBe(0);
-  })
+  });
 
-  it ('adding a new candy and checking total price of basket', () => {
-    // const candy = new Candy('Snickers', 3.99);
-
+  it("adding a new candy and checking total price of basket", () => {
     const fakeSnickers = { getPrice: () => 3.99 };
-    
-    const basket = new ShoppingBasket;
+    const basket = new ShoppingBasket();
     basket.addItem(fakeSnickers);
     expect(basket.getTotalPrice()).toBe(3.99);
-  })
+  });
 
-});  
+  it("applies discount to the total price of the basket", () => {
+    const fakeSnickers = { getPrice: () => 3.99 };
+    const basket = new ShoppingBasket();
+    basket.addItem(fakeSnickers);
+    basket.applyDiscount(2.00);
+    expect(basket.getTotalPrice()).toBe(1.99);
+  });
+});
